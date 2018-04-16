40c48c7e7f3856886a869784b7443424ff67657f
Przemek Jakubczyk <pjakubczyk@applause.com>
2018-04-16 16:08:37 +0200

    Read items from DB

--- app/src/main/java/org/jakubczyk/dbtesting/domain/interactor/GetTodoUseCase.java
+++ app/src/main/java/org/jakubczyk/dbtesting/domain/interactor/GetTodoUseCase.java
@@ -5,31 +5,37 @@ import android.support.annotation.NonNull;
 import org.jakubczyk.dbtesting.common.IoScheduler;
 import org.jakubczyk.dbtesting.common.MainScheduler;
 import org.jakubczyk.dbtesting.data.entity.TodoEntity;
+import org.jakubczyk.dbtesting.data.mapper.TodoItemMapper;
+import org.jakubczyk.dbtesting.db.model.TodoDbEntityEntity;
+import org.jakubczyk.dbtesting.domain.repository.datasource.TodoDatasource;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import rx.Observable;
+import rx.functions.Func1;
 
 public class GetTodoUseCase extends UseCase<List<TodoEntity>, Object> {
 
+    private final TodoDatasource todoDatasource;
+
     @Inject
-    GetTodoUseCase(@NonNull MainScheduler mainScheduler, @NonNull IoScheduler ioScheduler) {
+    GetTodoUseCase(
+            @NonNull MainScheduler mainScheduler,
+            @NonNull IoScheduler ioScheduler,
+            TodoDatasource todoDatasource
+    ) {
         super(mainScheduler, ioScheduler);
+        this.todoDatasource = todoDatasource;
     }
 
     @Override
     Observable<List<TodoEntity>> buildUseCaseObservable(@NonNull Object o) {
-        List<TodoEntity> todoEntities = new ArrayList<>(20);
-
-        for (int i = 0; i < 20; ++i) {
-            TodoEntity todoEntity = new TodoEntity("item " + i, new Date(System.currentTimeMillis() + i));
-            todoEntities.add(todoEntity);
-        }
-
-        return Observable.just(todoEntities);
+        return todoDatasource
+                .getTodoEntitiesStream()
+                .flatMap((Func1<List<TodoDbEntityEntity>, Observable<TodoDbEntityEntity>>) Observable::from)
+                .map(todoDbEntityEntity -> new TodoItemMapper(todoDbEntityEntity).toTodoEntity())
+                .toList();
     }
 }
--- app/src/test/groovy/org/jakubczyk/dbtesting/domain/interactor/GetTodoUseCaseSpec.groovy
+++ app/src/test/groovy/org/jakubczyk/dbtesting/domain/interactor/GetTodoUseCaseSpec.groovy
@@ -2,21 +2,36 @@ package org.jakubczyk.dbtesting.domain.interactor
 
 import org.jakubczyk.dbtesting.MySpecification
 import org.jakubczyk.dbtesting.data.entity.TodoEntity
+import org.jakubczyk.dbtesting.db.model.TodoDbEntityEntity
+import org.jakubczyk.dbtesting.domain.repository.datasource.TodoDatasource
+import rx.Observable
 import rx.observers.TestSubscriber
 
 class GetTodoUseCaseSpec extends MySpecification {
+
+    def todoDatasource = Mock(TodoDatasource)
 
     GetTodoUseCase getTodoUseCase
 
     def "setup"() {
         getTodoUseCase = new GetTodoUseCase(
                 testMainScheduler,
-                testIoScheduler
+                testIoScheduler,
+                todoDatasource
         )
     }
 
     def "should load 20 items"() {
         given:
+        def items = (1..20).collect {
+
+            def entity = new TodoDbEntityEntity()
+            entity.setItemValue("item" + it)
+            entity
+        }
+
+        todoDatasource.getTodoEntitiesStream() >> Observable.just(items)
+
         def testSubscriber = new TestSubscriber<List<TodoEntity>>()
 
         when:
@@ -27,5 +42,8 @@ class GetTodoUseCaseSpec extends MySpecification {
 
         then:
         testSubscriber.onNextEvents[0].size() == 20
+
+        and: "check first value"
+        testSubscriber.onNextEvents[0][0].getValue() == "item1"
     }
 }
